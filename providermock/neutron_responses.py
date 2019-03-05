ed6cf54bf6db8e17a15e464639ca0501bdd6acd2
Jeffrey Cutter <jcutter@redhat.com>
2019-03-05 11:48:44 -0500

    Enable post_networks to handle additional options.

--- providermock/neutron_responses.py
+++ providermock/neutron_responses.py
@@ -334,32 +334,28 @@ def post_networks(content, id):
     received_network = content_json['network']
     network = dict()
 
-#    #  The port id 'id' will be passed to the VIF driver as "vnic_id"
-#    if getattr(received_network, 'id', None):  # existing port is updated
-#        network_id = received_network['id']
-#    else:  # if port has no id, create a new one
-#        network_id = 'network_id_' + generate_id()
-#
-#    # only copy the relevant keys, fail if any of them is missing
-#    network['id'] = network_id
+    # only copy the relevant keys, fail if any of them is missing
     network['name'] = received_network['name']
-#
-#    if 'admin_state_up' in received_network:
-#        network['admin_state_up'] = received_network['admin_state_up']
-#
-#    if 'tenant_id' in received_network:
-#        network['tenant_id'] = received_network['tenant_id']
-#    if 'provider:physical_network' in received_network:
-#        network['provider:physical_network'] = received_network['provider:physical_network']
-#    # 'vlan' if a vlan network
-#    if 'provider:network_type' in received_network:
-#        network['provider:network_type'] = received_network['provider:network_type']
-#    # vlan segment
-#    if 'provider:segmentation_id' in received_network:
-#        network['provider:segmentation_id'] = received_network['provider:segmentation_id']
+
+    if 'admin_state_up' in received_network:
+        network['admin_state_up'] = received_network['admin_state_up']
+
+    if 'tenant_id' in received_network:
+        network['tenant_id'] = received_network['tenant_id']
+    if 'provider:physical_network' in received_network:
+        network['provider:physical_network'] = received_network['provider:physical_network']
+    # 'vlan' if a vlan network
+    if 'provider:network_type' in received_network:
+        network['provider:network_type'] = received_network['provider:network_type']
+    # vlan segment
+    if 'provider:segmentation_id' in received_network:
+        network['provider:segmentation_id'] = received_network['provider:segmentation_id']
 
     print "UPDATE NETWORK:" + str(network)
     obj = vnc_api.VirtualNetwork(network['name'])
+
+    ### Add vnc_api requirements to handle other key/value pairs in network dict here. ###
+
     network['id'] = vnc().virtual_network_create(obj)
     return json.dumps({'network': network})
 
